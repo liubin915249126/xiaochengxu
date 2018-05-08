7633ac8c1c93df992abbd8af3e29edd6f68c58a8
刘宾 <915249126@qq.com>
2018-05-08 16:42:45 +0800

    add demo

--- client/src/pages/request/index.vue
+++ client/src/pages/request/index.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    <input type="button" value="request" @click="request"/> 
+    <input type="button" value="request" @click="request"/>
+    <img :src='userInfo.avatarUrl' alt="avatar" v-if="userInfo.avatarUrl"/>
+    <input type="button" value="cgi" @click="testCgi"/>
   </div>
 </template>
 <script>
-// import request from '../../../utils/request'
+import request from '../../../utils/request'
 import config from '../../../config/config'
 const qcloud = require('../../vendor/wafer2-client-sdk/index')
 import { showBusy, showSuccess, showModel } from '../../utils'
@@ -12,7 +14,8 @@ import { showBusy, showSuccess, showModel } from '../../utils'
 export default {
   data () {
     return {
-      logged: false
+      logged: false,
+      userInfo: {}
     }
   },
   methods: {
@@ -25,19 +28,17 @@ export default {
     //   }
     // }
     request () {
-      // if (this.data.logged) return
+      if (this.logged) return
       showBusy('正在登录')
       var that = this
-
+      qcloud.setLoginUrl(config.service.loginUrl)
         // 调用登录接口
       qcloud.login({
         success (result) {
           if (result) {
             showSuccess('登录成功')
-            that.setData({
-              userInfo: result,
-              logged: true
-            })
+            that.userInfo = result
+            that.logged = true
           } else {
               // 如果不是首次登录，不会返回用户信息，请求用户信息接口获取
             qcloud.request({
@@ -45,10 +46,8 @@ export default {
               login: true,
               success (result) {
                 showSuccess('登录成功')
-                that.setData({
-                  userInfo: result.data.data,
-                  logged: true
-                })
+                that.userInfo = result.data.data
+                that.logged = true
               },
               fail (error) {
                 showModel('请求失败', error)
@@ -62,6 +61,25 @@ export default {
           console.log('登录失败', error)
         }
       })
+    },
+    testCgi () {
+      showBusy('请求中...')
+      request.get(`${config.service.host}/weapp/demo`)
+      // var that = this
+      // qcloud.request({
+      //   url: `${config.service.host}/weapp/demo`,
+      //   login: false,
+      //   success (result) {
+      //     showSuccess('请求成功完成')
+      //     that.setData({
+      //       requestResult: JSON.stringify(result.data)
+      //     })
+      //   },
+      //   fail (error) {
+      //     showModel('请求失败', error)
+      //     console.log('request fail', error)
+      //   }
+      // })
     }
   }
 }
--- client/utils/request.js
+++ client/utils/request.js
@@ -124,7 +124,7 @@ function checkCode(res) {
     // 如果code异常(这里已经包括网络错误，服务器错误，后端抛出的错误)，可以弹出一个错误提示，告诉用户
     if (res.status === -404) {
         let vue = new Vue;
-        vue.$message({ type: 'error', message:res.msg});
+        // vue.$message({ type: 'error', message:res.msg});
     }
     // if (res.data && (!res.data.success)) {
     //     alert(res.data.error_msg)
