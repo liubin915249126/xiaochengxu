6d0bda3d579ba04ce4302f385c8d819227d9d04e
liubin915249126 <915249126@qq.com>
2018-09-22 15:12:20 +0800

    modify

--- client/src/pages/todos/index.vue
+++ client/src/pages/todos/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div></div>
+  <div>
+    <div v-for="(item, index) in todos" :key="index">
+     {{index+1}}.{{item.name}}
+    </div>
+    <input type="button" value="add" @click="addItem"/>
+  </div>
 </template>
 <script>
 export default {
@@ -19,6 +24,23 @@ export default {
       }).get({
         success: res => {
           this.todos = res.data
+          console.log(res)
+        }
+      })
+    },
+    addItem () {
+      const db = wx.cloud.database()
+      db.collection('todos').add({
+        // data 字段表示需新增的 JSON 数据
+        data: {
+          descption: '11',
+          name: 'len',
+          fields: {
+
+          }
+        },
+        success: function (res) {
+          // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
           console.log(res)
         }
       })
