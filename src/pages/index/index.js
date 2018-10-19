74e29669afcd895a7bed0b2103920c61da34923e
liubin915249126 <915249126@qq.com>
2018-10-19 15:40:52 +0800

    自选商品下单

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,60 +1,10 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Button, Text } from '@tarojs/components'
-import { connect } from '@tarojs/redux'
 
-import { add, minus, asyncAdd } from '../../actions/counter'
+export default class Main extends Component{
+  render(){
+   return <View>
 
-import './index.less'
-
-
-@connect(({ counter }) => ({
-  counter
-}), (dispatch) => ({
-  add () {
-    dispatch(add())
-  },
-  dec () {
-    dispatch(minus())
-  },
-  asyncAdd () {
-    dispatch(asyncAdd())
-  }
-}))
-class Index extends Component {
-
-  config = {
-    navigationBarTitleText: '首页'
-  }
-
-  componentWillReceiveProps (nextProps) {
-    console.log(this.props, nextProps)
-  }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
-
-  goToLive () {
-    Taro.navigateTo({url:'../live/index'});
-    // Taro.redirectTo({
-    //   url: '../live/index'
-    // })
-  }
-
-  render () {
-    return (
-      <View className='index'>
-        <Button className='add_btn' onClick={this.props.add}>+</Button>
-        <Button className='dec_btn' onClick={this.props.dec}>-</Button>
-        <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
-        <View><Text>{this.props.counter.num}</Text></View>
-        <View><Text>Hello, World</Text></View>
-        <Button onClick={this.goToLive}>live</Button>
-      </View>
-    )
+    </View>
   }
 }
-
-export default Index
