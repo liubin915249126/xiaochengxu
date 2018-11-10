ebf093f5328f40bca3b3e9837f8d899b3a14b200
liubin915249126 <915249126@qq.com>
2018-11-10 14:52:24 +0800

    add

---
+++ remember.md
@@ -0,0 +1,6 @@
+#### taro
+[官网](https://nervjs.github.io/taro/docs/GETTING-STARTED.html)
+```bash
+   $ npm run dev:weapp
+   $ npm run build:weapp
+```
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,10 +1,32 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Button, Text } from '@tarojs/components'
+import { AtButton, AtAvatar } from 'taro-ui'
+import './index.less'
 
-export default class Main extends Component{
-  render(){
-   return <View>
-
+export default class Main extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      userInfo: {},
+      canIUse: wx.canIUse('button.open-type.getUserInfo')
+    }
+  }
+  getUserInfo (data) {
+    const { detail: { userInfo } } = data
+    this.setState({
+      userInfo
+    })
+  }
+  render () {
+    const { userInfo, canIUse } = this.state
+    return <View>
+      <View>
+        {userInfo.userAvatarUrl ? <AtAvatar image={userInfo.userAvatarUrl} /> : null}
+        <Text>申请获取你的公开信息（昵称、头像等)</Text>
+        <AtButton type='primary' open-type='getUserInfo' onGetUserInfo={this.getUserInfo} className='getUserInfo'> 微信授权 </AtButton>
+        {/* {canIUse?<Button type="primary" open-type='getUserInfo' onGetUserInfo={this.getUserInfo} className="getUserInfo"> 微信授权 </Button>:
+              <Button type="primary" open-type='getUserInfo' onGetUserInfo={this.getUserInfo} className="getUserInfo"> 11111</Button>} */}
+      </View>
     </View>
   }
 }
